549c2850598f600acc8eedc4356ecbdf37f815e3
Nirav49 <niravgabani9627@gmail.com>
2022-03-21 12:22:33 +0530

    list call

--- components/userList.js
+++ components/userList.js
@@ -3,10 +3,13 @@ import React from "react";
 const UserList = ({ list }) => {
   return list.map((item) => {
     return (
-      <React.Fragment key={item.id}>
-        <p style={{textAlign:"center"}}>Lists</p>
-        <p style={{textAlign:"center"}}>{item.id}</p>
-      </React.Fragment>
+      <ul key={item.id} style={{textAlign: 'center',listStyle:"none"}}>
+        <li>{item.id}</li>
+        <li>{item.name}</li>
+        <li>{item.username}</li>
+        <li>{item.email}</li>
+        <hr />
+      </ul>
     );
   });
 };
